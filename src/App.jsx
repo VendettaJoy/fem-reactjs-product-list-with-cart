9a699de8b7eb480e2919c070be14f906145a31bd
Vendetta Joy <44125834+VendettaJoy@users.noreply.github.com>
2025-07-16 15:35:37 +0530

    Use axios to fetch list data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+import ProductList from "./components/ProductList/ProductList";
+import axios from "axios";
+
 const App = () => {
-	return <div>App</div>;
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		const getMenu = async () => {
+			try {
+				const response = await axios.get("/db/data.json");
+				const menu = response.data;
+				setList(menu);
+			} catch (error) {
+				console.log(error.response.data);
+				console.log(error.response.status);
+				console.log(error.response.headers);
+			}
+		};
+
+		getMenu();
+	}, []);
+
+	return (
+		<main>
+			<ProductList list={list} />
+		</main>
+	);
 };
 export default App;
