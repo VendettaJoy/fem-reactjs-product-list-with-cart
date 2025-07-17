8e54b8d82a2d327457b8f0a9d66a7a5a319be26b
Vendetta Joy <44125834+VendettaJoy@users.noreply.github.com>
2025-07-17 13:09:08 +0530

    Add Heading

--- src/components/List/ProductList/ProductList.jsx
+++ src/components/List/ProductList/ProductList.jsx
@@ -2,18 +2,21 @@ import ProductListItem from "../ProductListItem/ProductListItem";
 
 const ProductList = ({ list }) => {
 	return (
-		<ul>
-			{list.map((item, index) => (
-				<ProductListItem
-					key={index}
-					id={index}
-					image={item.image}
-					category={item.category}
-                    name={item.name}
-                    price={item.price}
-				/>
-			))}
-		</ul>
+		<>
+            <h1>Desserts</h1>
+			<ul>
+				{list.map((item, index) => (
+					<ProductListItem
+						key={index}
+						id={index}
+						image={item.image}
+						category={item.category}
+						name={item.name}
+						price={item.price}
+					/>
+				))}
+			</ul>
+		</>
 	);
 };
 export default ProductList;
