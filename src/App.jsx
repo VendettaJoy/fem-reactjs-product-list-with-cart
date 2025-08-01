1508edc303a96635a5baefe1a4e72f68ed915db2
Vendetta Joy <44125834+VendettaJoy@users.noreply.github.com>
2025-08-01 16:38:04 +0530

    Update App Styles
    Add App class to the main tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import CartProvider from "./context/CartContext";
 import ProductList from "./components/List/ProductList";
 import Cart from "./components/Cart/Cart";
 import CheckoutModal from "./components/Modal/CheckoutModal";
+import "./App.css"
 
 const App = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	return (
 		<CartProvider>
-			<main>
+			<main className="App">
 				<ProductList />
 				<Cart setIsModalOpen={setIsModalOpen} />
 				<CheckoutModal
